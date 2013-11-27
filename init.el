8049b604da1b290f105540be298f7d533a50ec4f
Konstantin Burnaev <kbourn@gmail.com>
2013-11-28 07:25:57 +1100

    Javascript mode configuration.

--- init.el
+++ init.el
@@ -326,6 +326,19 @@
 
 (add-hook 'js-mode-hook 'flymake-jshint-load)
 (add-hook 'js-mode-hook 'flymake-jslint-load)
+(setq-default js2-indent-on-enter-key t
+              js2-auto-indent-p t
+              js2-bounce-indent-p t
+              ;; Idle timeout before reparsing buffer
+              js2-idle-timer-delay 0.5
+              ;; load browser-specific functions
+              js2-include-browser-externs t
+              ;; Support Node.js
+              js2-include-node-externs t
+              js2-skip-preprocessor-directives t
+              ;; Disable error parsing in favor of Flycheck
+              js2-show-parse-errors nil
+              js2-strict-missing-semi-warning nil)
 
 ;; ==== JSON
 
