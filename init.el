c9747e17f2afb2aa1460c5712ae028cc74e6f59c
Konstantin Burnaev <kbourn@gmail.com>
2014-01-13 23:44:11 +1100

    Added rbenv paths to editor executable paths.

--- init.el
+++ init.el
@@ -96,6 +96,21 @@
                 (package-install package)))
           packages)))
 
+;; == Paths and tools
+
+;; Setting rbenv path
+(setenv "PATH"
+        (concat
+         (getenv "HOME") "/.rbenv/shims:"
+         (getenv "HOME") "/.rbenv/bin:"
+         (getenv "PATH")))
+(setq exec-path
+      (cons
+       (concat (getenv "HOME") "/.rbenv/shims")
+       (cons
+        (concat (getenv "HOME") "/.rbenv/bin")
+        exec-path)))
+
 ;; == Key bindings
 
 ;; Move between windows using Shift + Arrows
