f752f27f91ef8786e42383218e89365924cc757c
Konstantin Burnaev <kbourn@gmail.com>
2014-02-17 08:18:28 +1100

    Magit now sets upstream branch automatically when I push new branch to the repo.

--- packages/magit.el
+++ packages/magit.el
@@ -1,5 +1,6 @@
 ;; magit
 ;; https://github.com/magit/magit
 
-(setq-default magit-diff-refine-hunk t)
-(setq-default magit-status-buffer-switch-function 'switch-to-buffer)
+(setq-default magit-diff-refine-hunk t
+              magit-set-upstream-on-push t
+              magit-status-buffer-switch-function 'switch-to-buffer)
