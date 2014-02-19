3e951d81c4335fc63d69ffe206fbd4eb6b81984b
Konstantin Burnaev <kbourn@gmail.com>
2014-02-20 07:41:54 +1100

    Fixed config file for the diminish package.

--- packages/diminish.el
+++ packages/diminish.el
@@ -1,22 +1,25 @@
 ;; diminish
 ;; http://www.emacswiki.org/emacs/DiminishedModes
 
-(mapc #'(lambda (mode)
-	  (eval-after-load
-	      (symbol-name mode)
-	    (lambda () (diminish mode))))
-      '(
-        abbrev-mode
-        anzu-mode
-        auto-complete-mode
-        auto-fill-function
-        fic-mode
-        flymake-mode
-        flyspell-mode
-        git-gutter+-mode
-        helm-mode
-        page-break-lines-mode
-        projectile-mode
-        robe-mode
-        yas-minor-mode
-        ))
+(defun bkon/diminish-after-load (package-name mode)
+  (eval-after-load package-name
+    `(diminish ,mode)))
+
+(mapc
+ #'(lambda
+     (data)
+     (bkon/diminish-after-load (car data) (cdr data)))
+ '(
+   (abbrev . 'abbrev-mode)
+   (anzu . 'anzu-mode)
+   (auto-complete . 'auto-complete-mode)
+   (auto-fill . 'auto-fill-function)
+   (fic-mode . 'fic-mode)
+   (flymake . 'flymake-mode)
+   (flyspell . 'flyspell-mode)
+   (helm . 'helm-mode)
+   (git-gutter+ . 'git-gutter+-mode)
+   (page-break-lines . 'page-break-lines-mode)
+   (projectile . 'projectile-mode)
+   (robe . 'robe-mode)
+   (yasnippet . 'yas-minor-mode)))
