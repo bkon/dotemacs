607dee52f02315a14500c0629d5baccd42fab955
Konstantin Burnaev <kbourn@gmail.com>
2014-08-24 19:59:08 +1000

    Minor changes in Ag mode key bindings.

--- packages/helm-ag-r.el
+++ packages/helm-ag-r.el
@@ -2,4 +2,5 @@
 ;; https://github.com/yuutayamada/helm-ag-r
 
 (global-set-key (kbd "C-x a a") 'helm-ag-r-from-git-repo)
+(global-set-key (kbd "C-x a d") 'helm-ag-r)
 (setq helm-ag-r-input-idle-delay nil)
