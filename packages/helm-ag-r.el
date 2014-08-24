ca0fe79f57b5c4333748118a60c4044f652f62b5
Konstantin Burnaev <Konstantin.Burnaev@vodafone.com.au>
2014-08-24 14:24:24 +1000

    Changes in helm-ag key bindings.

--- packages/helm-ag-r.el
+++ packages/helm-ag-r.el
@@ -1,4 +1,5 @@
 ;; helm-ag-r
 ;; https://github.com/yuutayamada/helm-ag-r
 
-(global-set-key (kbd "C-x a a") 'helm-ag-r)
+(global-set-key (kbd "C-x a a") 'helm-ag-r-from-git-repo)
+(setq helm-ag-r-input-idle-delay nil)
