61b19aa6b0914fd2ddae29fad3fc05967bf0cf9f
Konstantin Burnaev <kbourn@gmail.com>
2015-03-01 10:29:28 +1100

    [master] DWIM behavior for C-<backspace> - delete just whitespace (often used to erase empty lines in source code)

--- packages/hungry-delete.el
+++ packages/hungry-delete.el
@@ -3,4 +3,13 @@
 
 (require 'hungry-delete)
 
-(global-hungry-delete-mode)
+(defun bkon/dwim-backward-kill-word (arg)
+  (interactive "p")
+  (let ((previous-char-is-space-or-newline
+         (member (char-before) '(?\n ?\s))
+         ))
+    (if previous-char-is-space-or-newline
+        (hungry-delete-backward arg nil)
+      (backward-kill-word arg))))
+
+(global-set-key (kbd "<C-backspace>") 'bkon/dwim-backward-kill-word)
