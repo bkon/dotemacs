92d856ad7c8db80d75c67ed7b3e3bf012fdc69bf
Konstantin Burnaev <kon@rangeme.com.au>
2015-08-27 09:39:01 +1000

    Better prefix generation for the commit messages.

--- packages/magit.el
+++ packages/magit.el
@@ -6,20 +6,27 @@
               magit-status-buffer-switch-function 'switch-to-buffer
               magit-save-some-buffers nil)
 
+(defun bkon/magit/get-issue-number (&rest discard)
+  "Extract current issue number from the branch name"
+  (interactive)
+  (when (string-match "^[0-9]+" (magit-get-current-branch))
+    (match-string 0 (magit-get-current-branch))))
+
 (defun bkon/magit/add-branch-to-commit-message (&rest discard)
   "Add a current branch name to the default commit message"
   (unless current-prefix-arg ;; ignore commit amends
-    (if (magit-get-current-branch)
-        (let ((tag (format "[%s] " (magit-get-current-branch))))
+    (if (bkon/magit/get-issue-number)
+        (let ((tag (format "[%s] " (bkon/magit/get-issue-number))))
           (goto-char (point-min))
           (unless (search-forward tag nil t) (insert tag))))))
 
 (defun bkon/magit/add-newline-to-commit-message (&rest discard)
   "Add newline to a commit message, so magit won't complain about the second line"
-  (unless current-prefix-arg ;; ignore commit amends
-    (search-forward "#")
-    (backward-char)
-    (insert "\n")))
+  (if (bkon/magit/get-issue-number)
+      (unless current-prefix-arg ;; ignore commit amends
+        (search-forward "#")
+        (backward-char)
+        (insert "\n"))))
 
 
 (add-hook 'git-commit-mode-hook 'bkon/magit/add-branch-to-commit-message)
