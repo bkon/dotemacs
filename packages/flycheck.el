6bcfab241f5c587684feb22cce003be79a33dd33
Konstantin Burnaev <kon@rangeme.com.au>
2015-08-27 08:56:57 +1000

    Configured eslint checked to run in jsx-mode

--- packages/flycheck.el
+++ packages/flycheck.el
@@ -1,16 +1,4 @@
 ;; flycheck
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
-
-(require 'flycheck)
-(flycheck-define-checker jsxhint-checker
-  "A JSX syntax and style checker based on JSXHint."
-
-  :command ("jsxhint" source)
-  :error-patterns
-  ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
-  :modes (jsx-mode))
-
-(add-hook 'jsx-mode-hook (lambda ()
-                          (flycheck-select-checker 'jsxhint-checker)
-                          (flycheck-mode)))
+(flycheck-add-mode 'javascript-eslint 'jsx-mode)
