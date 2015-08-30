102e021f16f47b8c717e3f535466661315a1f968
Konstantin Burnaev <kbourn@gmail.com>
2015-08-30 21:24:30 +1000

    jsx-mode / electring indent.
    jsx-mode has been turned off.
    Electring indent has been turned off.

--- init.el
+++ init.el
@@ -23,6 +23,7 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (fringe-mode '(10 . 10))
+(electric-indent-mode nil)
 
 (require 'cl)
 
@@ -410,6 +411,7 @@ Including indent-buffer, which should not be called automatically on save."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flycheck-disabled-checkers (quote (javascript-jshint)))
  '(js2-basic-offset 2)
  '(magit-branch-arguments nil)
  '(magit-diff-arguments (quote ("--ignore-space-change" "-M50%" "-C50%")))
