42cabad0c9c2f5cf5a55049a6748a6dc546ec103
Konstantin Burnaev <kon@rangeme.com.au>
2016-01-11 17:16:41 +1100

    Flow check takes way too long.

--- packages/flycheck.el
+++ packages/flycheck.el
@@ -3,4 +3,6 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (require 'flycheck)
+;; (require 'flycheck-flow)
 (flycheck-add-mode 'javascript-eslint 'jsx-mode)
+;; (flycheck-add-next-checker 'javascript-eslint 'javascript-flow)
