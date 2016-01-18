5b5d4f35f28805c8f9710da853cd7f787abe0e04
Konstantin Burnaev <kon@rangeme.com.au>
2016-01-18 11:12:58 +1100

    Default mode for .babelrc

--- packages/js2-mode.el
+++ packages/js2-mode.el
@@ -4,6 +4,7 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '(".eslintrc" . js2-mode))
+(add-to-list 'auto-mode-alist '(".babelrc" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 (setq-default js2-indent-on-enter-key nil
