de44e6acd98b7841022ad8fe0e3390a225bed495
Konstantin Burnaev <c_kburna@qti.qualcomm.com>
2018-04-25 11:34:46 +1000

    Better handling of tslint and eslint

--- packages/flycheck.el
+++ packages/flycheck.el
@@ -4,5 +4,23 @@
 
 (require 'flycheck)
 ;; (require 'flycheck-flow)
+
+;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let ((root (locate-dominating-file
+               (or (buffer-file-name) default-directory)
+               (lambda (dir)
+                 (let ((eslint (expand-file-name "node_modules/eslint/bin/eslint.js" dir)))
+                  (and eslint (file-executable-p eslint)))))))
+    (when root
+      (let ((eslint (expand-file-name "node_modules/eslint/bin/eslint.js" root)))
+        (setq-local flycheck-javascript-eslint-executable eslint)))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+(flycheck-def-config-file-var flycheck-typescript-tsconfig
+    typescript-tslint "tslint.json"
+  :safe #'stringp
+  :package-version '(flycheck . "27"))
+
 (flycheck-add-mode 'javascript-eslint 'jsx-mode)
-;; (flycheck-add-next-checker 'javascript-eslint 'javascript-flow)
